7c5e550107518a69d669bab39f483c2836e79cd3
eugene <eu.emets@gmail.com>
2022-08-18 15:23:03 +0300

    my version of game

--- index.js
+++ index.js
@@ -7,9 +7,88 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const habitats = [];
+const PROPS = [
+  "species",
+  "name",
+  "gender",
+  "legs",
+  "hands",
+  "saying",
+  "friends",
+];
+const SPECIES = { DOG: "dog", CAT: "cat", HUMAN: "human" };
+const GENDER = { MALE: "male", FEMALE: "female" };
+let createHabitat = (species, name, gender, legs, hands, saying) => {
+  const data = {
+    species,
+    name,
+    gender,
+    legs,
+    hands,
+    saying,
+    friends: [],
+    addFriend,
+    toString: () => {
+      let info = [species, name, gender, legs, hands, saying].join("; ");
+      if (data.friends.length > 0) {
+        info += "; ";
+        let friendList = data.friends.reduce((list, friend) => {
+          list.push(friend.species + " " + friend.name);
+          return list;
+        }, []);
 
+        info += friendList.join(", ");
+      }
+      return info;
+    },
+  };
+  return data;
+};
 
+const dog = createHabitat(SPECIES.DOG, "Sharik", GENDER.MALE, 4, 0, "gav-gav!");
+const cat = createHabitat(SPECIES.CAT, "Murka", GENDER.FEMALE, 4, 0, "Myau");
+const man = createHabitat(
+  SPECIES.HUMAN,
+  "Igor",
+  GENDER.MALE,
+  2,
+  2,
+  "Hello, everebody!"
+);
+
+const woman = createHabitat(
+  SPECIES.HUMAN,
+  "Nata",
+  GENDER.FEMALE,
+  2,
+  2,
+  "Hi. My name is Nata"
+);
+
+const catWoman = createHabitat(
+  SPECIES.HUMAN,
+  "Margo",
+  GENDER.FEMALE,
+  2,
+  2,
+  cat.saying
+);
+
+function addFriend(friend) {
+  this.friends.push(friend);
+  friend.friends.push(this);
+}
+
+dog.addFriend(cat);
+man.addFriend(cat);
+man.addFriend(woman);
+habitats.push(dog, cat, man, woman, catWoman);
 // ======== OUTPUT ========
+habitats.forEach((habitat) => {
+  print(habitat);
+});
+
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
@@ -27,5 +106,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
