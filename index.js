44a43f9c250cfe52fa3eefc1724a70619fbe0601
eugene <eu.emets@gmail.com>
2022-08-20 06:18:16 +0300

    iteration #1

--- index.js
+++ index.js
@@ -19,21 +19,34 @@ const PROPS = [
 ];
 const SPECIES = { DOG: "dog", CAT: "cat", HUMAN: "human" };
 const GENDER = { MALE: "male", FEMALE: "female" };
-let createHabitat = (species, name, gender, legs, hands, saying) => {
-  const data = {
-    species,
-    name,
-    gender,
-    legs,
-    hands,
-    saying,
-    friends: [],
-    addFriend,
-    toString: () => {
-      let info = [species, name, gender, legs, hands, saying].join("; ");
-      if (data.friends.length > 0) {
+class Habitat {
+  friends=[];
+  constructor (species,name,gender,legs/*,hands,*/,saying){
+
+  this.species = species;
+  this.name = name;
+  this.gender = gender;
+  this.legs = legs;
+  //this.hands = hands;
+  this.saying = saying;
+
+  }
+
+addFriend (habitat){
+  if (!this.friends.includes (habitat)){
+    this.friends.push(habitat);
+  }
+  if (!habitat.friends.includes (this)){
+    habitat.friends.push (this);
+  }
+}
+
+toString (){
+
+      let info = [this.species, this.name, this.gender, this.legs,'0', this.saying].join("; ");
+      if (this.friends.length > 0) {
         info += "; ";
-        let friendList = data.friends.reduce((list, friend) => {
+        let friendList = this.friends.reduce((list, friend) => {
           list.push(friend.species + " " + friend.name);
           return list;
         }, []);
@@ -41,15 +54,62 @@ let createHabitat = (species, name, gender, legs, hands, saying) => {
         info += friendList.join(", ");
       }
       return info;
-    },
-  };
-  return data;
-};
+    }
+}
 
-const dog = createHabitat(SPECIES.DOG, "Sharik", GENDER.MALE, 4, 0, "gav-gav!");
-const cat = createHabitat(SPECIES.CAT, "Murka", GENDER.FEMALE, 4, 0, "Myau");
-const man = createHabitat(
-  SPECIES.HUMAN,
+
+class Animal extends Habitat
+{
+  
+constructor (species,name,gender,legs,saying)
+  {
+    super(species,name,gender,legs,saying)
+  }
+
+}
+
+class Human extends Habitat{
+  constructor (name,gender,legs,hands,saying){
+  super(SPECIES.HUMAN,name, gender,legs,saying);
+  this.hands = hands;
+  
+  }
+  
+toString (){
+
+      let info = [this.species, this.name, this.gender, this.legs,this.hands, this.saying].join("; ");
+      if (this.friends.length > 0) {
+        info += "; ";
+        let friendList = this.friends.reduce((list, friend) => {
+          list.push(friend.species + " " + friend.name);
+          return list;
+        }, []);
+
+        info += friendList.join(", ");
+      }
+      return info;
+    }
+}
+
+class Dog extends Animal {
+constructor (name,gender,legs,saying){
+  super (SPECIES.DOG,name,gender,legs,saying)
+
+}
+}
+
+class Cat extends Animal {
+constructor (name,gender,legs,saying){
+  super (SPECIES.CAT,name,gender,legs,saying)
+
+}
+}
+
+
+
+const dog = new Dog("Sharik", GENDER.MALE, 4, "gav-gav!");
+const cat = new Cat("Murka", GENDER.FEMALE, 4, "Myau");
+const man = new Human(
   "Igor",
   GENDER.MALE,
   2,
@@ -57,8 +117,7 @@ const man = createHabitat(
   "Hello, everebody!"
 );
 
-const woman = createHabitat(
-  SPECIES.HUMAN,
+const woman = new Human(
   "Nata",
   GENDER.FEMALE,
   2,
@@ -66,8 +125,7 @@ const woman = createHabitat(
   "Hi. My name is Nata"
 );
 
-const catWoman = createHabitat(
-  SPECIES.HUMAN,
+const catWoman = new Human(
   "Margo",
   GENDER.FEMALE,
   2,
@@ -75,11 +133,8 @@ const catWoman = createHabitat(
   cat.saying
 );
 
-function addFriend(friend) {
-  this.friends.push(friend);
-  friend.friends.push(this);
-}
 
+dog.addFriend(cat);
 dog.addFriend(cat);
 man.addFriend(cat);
 man.addFriend(woman);
